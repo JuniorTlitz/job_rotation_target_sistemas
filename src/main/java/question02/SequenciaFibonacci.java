331e7259db17f6995335a989b9d2cdfdb44216c2
Tarcizio Junior <62338122+JuniorTlitz@users.noreply.github.com>
2023-04-11 17:40:09 -0300

    Question02: chama o método isFibonacci e imprime no log, se pertence ou não, na Sequencia Fibonacci

--- src/main/java/question02/SequenciaFibonacci.java
+++ src/main/java/question02/SequenciaFibonacci.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 
 public class SequenciaFibonacci {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número para verificar se pertence à sequência de Fibonacci: ");
+        int num = scanner.nextInt();
+        scanner.close();
 
+        if (isFibonacci(num)) {
+            System.out.println(num + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println(num + " não pertence à sequência de Fibonacci.");
+        }
     }
     public static boolean isFibonacci(int num) {
         int a = 0;
